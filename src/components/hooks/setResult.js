a92147f09bc02d8e3ee77fafecfc9208b16d77c9
Arul-1911 <arulpraveen19112002@gmail.com>
2024-04-11 17:49:56 +0530

    modified username section

--- src/components/hooks/setResult.js
+++ src/components/hooks/setResult.js
@@ -24,6 +24,7 @@ export const usePublishResult = (resultData) => {
   const { result, username } = resultData;
   (async () => {
     try {
+       if (!username) throw new Error("Couldn't get Result");
       await postServerData(
         `${backend_url}/api/result`,
         resultData,
